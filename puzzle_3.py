723dc40aafc5610e868fcb3d1df2ba0d06bc5368
Jon Watts <jonathanwatts@mac.com>
2025-01-13 16:48:44 +0000

    add puzzle 2

---
+++ puzzle_3.py
@@ -0,0 +1,13 @@
+def sum_even_numbers(list_of_numbers):
+    sum = 0
+    for number in list_of_numbers:
+        if number % 2 == 0:
+            sum += number
+    return sum
+
+
+print(sum_even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(sum_even_numbers([2, 4, 6, 8, 10]))
+print(sum_even_numbers([1, 3, 5, 7, 9]))
+print(sum_even_numbers([10, 20, 30, 40, 50]))
+print(sum_even_numbers([15, 25, 35, 45, 55]))
