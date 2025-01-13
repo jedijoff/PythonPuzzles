66714f98064863c4c181d96fcbb81144522e4867
Jon Watts <jonathanwatts@mac.com>
2025-01-13 21:59:21 +0000

    Update puzzle_004.py
    Add Shell shebang line and docstring.

--- puzzle_004.py
+++ puzzle_004.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3
+
 def remove_vowels(input_string):
+    """returns a string with all vowels removed.
+
+    :param input_string: a user input string.
+    :return: the input string with all vowels removed.
+    """
     vowels = "aeiou"
-    return "".join([char for char in input_string if char.lower() not in vowels])
+    return "".join([chr for chr in input_string if chr.lower() not in vowels])
 
 
 print(remove_vowels("Apple"))
 print(remove_vowels("banana"))
 print(remove_vowels("cherry"))
 print(remove_vowels("Hello world"))
-print(remove_vowels("Echoes"))
+print(remove_vowels("Echo"))
