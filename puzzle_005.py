979e67ed03c982064794f47226dc87486a4ad36d
Jon Watts <jonathanwatts@mac.com>
2025-01-13 22:19:30 +0000

    Add shell shebang lines and docstrings.

--- puzzle_005.py
+++ puzzle_005.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python3
+
 def get_longest_string(input_strings):
+    """ A function that returns the longest string from a list of strings
+
+    Args:
+        input_strings: A list of strings. The list must contain
+        a minimum of 1 string"""
     longest_str = ''
     for word in input_strings:
         if len(word) > len(longest_str):
@@ -6,5 +13,7 @@ def get_longest_string(input_strings):
     return longest_str
 
 
-print(get_longest_string(['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape']))
-print(get_longest_string(['cat', 'dog', 'elephant', 'fish', 'giraffe', 'hippopotamus', 'iguana']))
+print(get_longest_string(['apple', 'banana', 'cherry', 'date',
+                          'elderberry', 'fig', 'grape']))
+print(get_longest_string(['cat', 'dog', 'elephant', 'fish',
+                          'giraffe', 'hippopotamus', 'iguana']))
