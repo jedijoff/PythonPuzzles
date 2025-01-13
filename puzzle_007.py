2d5a619c92143d2e19eae3502cb1bb5faf1acc21
Jon Watts <jonathanwatts@mac.com>
2025-01-13 22:49:49 +0000

    tidy function call examples

--- puzzle_007.py
+++ puzzle_007.py
@@ -1,6 +1,16 @@
+#!/usr/bin/env python3
+
 def reverse_elements(list_of_numbers):
+    """
+    Takes a user list of numbers, as input, returns the list reversed
+
+    :param list_of_numbers: a list of numbers.
+    :return: the list, reversed.
+    """
     return list_of_numbers[::-1]
 
 
+# Example function calls
 print(reverse_elements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 print(reverse_elements([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))
+print(reverse_elements([1.97, 2.97, 3.97, 4.97, 5.97, 6.97, 7.97]))
--- puzzle_008.py
+++ puzzle_008.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
+
 def filter_type_str(input_list):
+    """
+    Takes a list of elements, returns a list of only the string elements
+
+    :param input_list: a list of various elements.
+    :return: a list of all elements that are strings.
+    """
     return [i for i in input_list if type(i) == str]
 
 
-print(filter_type_str([1, 2, 3, 'a', 'b', 'c', 4, 5, 6]))
+# Example function calls:
+print(filter_type_str([1, 2, 3, 'a', 'b', 'c', 4, '5', 6]))
 print(filter_type_str([1, 2, 3, 4, 5, 6]))
 print(filter_type_str(['a', 'b', 'c']))
 print(filter_type_str([]))
+print(filter_type_str(['a', 1, (1, 'a'), 'b', 2, 'c', 3, ['cat', 1, '@']]))
