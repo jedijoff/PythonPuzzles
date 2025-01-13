479e5e827ddc906e5edde935305aa9af6307b472
Jon Watts <jonathanwatts@mac.com>
2025-01-13 21:10:15 +0000

    Update puzzle_004.py
    Lowercase char before check.

--- puzzle_004.py
+++ puzzle_004.py
@@ -1,9 +1,10 @@
 def remove_vowels(input_string):
     vowels = "aeiou"
-    return "".join([char for char in input_string if char not in vowels])
+    return "".join([char for char in input_string if char.lower() not in vowels])
 
 
-print(remove_vowels("apple"))
+print(remove_vowels("Apple"))
 print(remove_vowels("banana"))
 print(remove_vowels("cherry"))
 print(remove_vowels("Hello world"))
+print(remove_vowels("Echoes"))
