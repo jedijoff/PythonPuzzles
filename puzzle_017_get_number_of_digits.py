a1442a39bd3ba4ad2b5036c5babe1be49382471f
Jon Watts <jonathanwatts@mac.com>
2025-01-14 17:35:33 +0000

    Add a note to the docstring about the puzzle specifications.

--- puzzle_017_get_number_of_digits.py
+++ puzzle_017_get_number_of_digits.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-def get_number_of_digits(number):
+def get_number_of_digits(number, count=0):
     """
     Function returns the number of digits in a number.
 
     :param number: a number passed to the function.
     :return: the number of digits in the number.
+
+    NB: The puzzle specified that the function should be
+    recursive, and not convert the number to a string,
+    so the obvious return len(str(number)) option was
+    off the cards
     """
-    count = 0
-    P
-    while number > 0:
-        count += 1
+    if number > 0:
         number = number // 10
+        get_number_of_digits(number, count+1)
+    else:
+        print(count)
 
-    return count
 
-
-print(get_number_of_digits(1000))
-print(get_number_of_digits(1000000))
-print(get_number_of_digits(10000000))
-print(get_number_of_digits(1234567890))
+get_number_of_digits(1000)
+get_number_of_digits(1000000)
+get_number_of_digits(10000000)
+get_number_of_digits(1234567890)
