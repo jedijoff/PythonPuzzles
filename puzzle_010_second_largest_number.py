2873e720518c68313ba9f4f3d4adb822de4ff705
Jon Watts <jonathanwatts@mac.com>
2025-01-14 17:47:03 +0000

    Update puzzle_010_second_largest_number.py
    Add comment to examples.

--- puzzle_010_second_largest_number.py
+++ puzzle_010_second_largest_number.py
@@ -14,6 +14,7 @@ def second_largest_number(numbers: list):
     return (sorted(unique_numbers)[-2]) if len(unique_numbers) > 1 else None
 
 
+# Example function calls:
 print(second_largest_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 print(second_largest_number([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))
 print(second_largest_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
