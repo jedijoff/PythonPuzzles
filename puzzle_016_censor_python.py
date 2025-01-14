9857a799079b7f84ee587bf62f309dbdf782fb13
Jon Watts <jonathanwatts@mac.com>
2025-01-14 16:14:12 +0000

    Add solution to puzzle 16.

---
+++ puzzle_016_censor_python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def censor_python(string):
+    """
+    Function censors the word 'Python' with 'X's.
+
+    :param string: a string passed to the function.
+    :return: the string with the letters from Python censored.
+    """
+    python = ['P', 'y', 't', 'h', 'o', 'n']
+    for char in string:
+        if char in python:
+            string = string.replace(char, 'X')
+    return string
+
+
+print(censor_python("Hello Python! You're my favourite programming language!"))
+
