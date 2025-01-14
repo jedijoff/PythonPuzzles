70feaf8e3bb4cde7ff93c2e74021c3c1792b20fa
Jon Watts <jonathanwatts@mac.com>
2025-01-14 16:04:23 +0000

    Add solution to puzzle 15.

---
+++ puzzle_015_filter palindromes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def filter_palindromes(list_of_strings):
+    """
+    Returns strings from a list that are palindromes.
+
+    :param:
+        list_of_strings: A list of strings.
+    :return:
+        a new list containing only the strings that are palindromes.
+    """
+    palindrome_list = []
+    for word in list_of_strings:
+        if word == word[::-1]:
+            palindrome_list.append(word)
+    return palindrome_list
+
+
+print(filter_palindromes(['apple', 'banana', 'civic', 'date', 'racecar']))
