5438265b4152306c55aad2f68320abb7fdee2ac5
Jon Watts <jonathanwatts@mac.com>
2025-01-14 16:47:41 +0000

    Amend docstring in puzzle 17

---
+++ puzzle_017_get_number_of_digits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def get_number_of_digits(number):
+    """
+    Function returns the number of digits in a number.
+
+    :param number: a number passed to the function.
+    :return: the number of digits in the number.
+    """
+    count = 0
+    P
+    while number > 0:
+        count += 1
+        number = number // 10
+
+    return count
+
+
+print(get_number_of_digits(1000))
+print(get_number_of_digits(1000000))
+print(get_number_of_digits(10000000))
+print(get_number_of_digits(1234567890))
