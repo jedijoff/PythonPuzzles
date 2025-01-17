8e39484c696e56937cb14a9a65f46facfea432f4
Jon Watts <jonathanwatts@mac.com>
2025-01-17 00:32:33 +0000

    Simplify solution to puzzle 25.

---
+++ puzzle_025_is_valid_equation.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python3
+
+def is_valid_equation(input_equation: str) -> bool:
+    """
+    This function checks if an equation is valid.
+    :param: input_ equation: This is a string representing a simple
+            equation with a single operator and two operand, an equals
+            sign and an answer to be checked.
+    :return: True if the equation is valid, False otherwise.
+    """
+    split_equation = input_equation.split('=')
+    return eval(split_equation[0]) == int(split_equation[1])
+
+
+# Example function calls
+print(is_valid_equation('1 + 1 = 2'))
+print(is_valid_equation('1 + 1 = 3'))
+print(is_valid_equation('1 - 1 = 0'))
+print(is_valid_equation('10 * 10 = 101'))
+print(is_valid_equation('10 / 10 + 2 = 3'))
