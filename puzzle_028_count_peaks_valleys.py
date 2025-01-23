ce433129461155e5a85e02162349d379b04b35ce
jedijoff <jonathanwatts@mac.com>
2025-01-23 16:09:32 +0000

    Add extra example.

--- puzzle_028_count_peaks_valleys.py
+++ puzzle_028_count_peaks_valleys.py
@@ -28,3 +28,4 @@ def count_peaks_valleys(price_action: list[int]) -> tuple[int, int]:
 # Example function calls
 print(count_peaks_valleys([1, 2, 3, 4, 3, 2, 1]))
 print(count_peaks_valleys([1, 2, 3, 2, 1, 2]))
+print(count_peaks_valleys([7, 6, 5, 10, 11, 12, 10, 9, 10]))
